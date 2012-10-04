10969316520dfb06434a03066484346f8685a9d3
Dave Lambley <davel@state51.co.uk>
2012-10-04 15:07:22 +0000

    First chunk of logic.

--- lib/MogileFS/Plugin/FileRefs.pm
+++ lib/MogileFS/Plugin/FileRefs.pm
@@ -1,37 +1,66 @@
 package MogileFS::Plugin::FileRefs;
 
-use 5.010001;
 use strict;
 use warnings;
 
-require Exporter;
-use AutoLoader qw(AUTOLOAD);
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use MogileFS::Plugin::FileRefs ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
+sub load {
+    MogileFS::register_worker_command('add_file_ref', sub {
+        my ($query, $dmid, $dkey, $ref) = @_;
+        my $dbh = Mgd::get_dbh();
+        local $@;
+        my $updated = eval { $dbh->do("REPLACE INTO file_ref (dmid, dkey, ref) VALUES (?, ?, ?)", {}, $dmid, $dkey, $ref); };
+        if ($@ || $dbh->err) {
+            return $query->err_line("add_file_ref_fail");
+        }
 
-# Preloaded methods go here.
+        return $query->ok_line({made_new_ref => $updated});
+    }) or die;
 
-# Autoload methods go after =cut, and are processed by the autosplit program.
+    MogileFS::register_worker_command('del_file_ref', sub {
+        my ($query, $dmid, $dkey, $ref) = @_;
+        my $dbh = Mgd::get_dbh();
+        local $@;
+        my $deleted = eval { $dbh->do("DELETE file_ref WHERE dmid = ? AND dkey = ? AND ref = ?", {}, $dmid, $dkey, $ref) };
+        if ($@ || $dbh->err) {
+            return $query->err_line("del_file_ref_fail");
+        }
+        return $query->ok_line({deleted_ref => $deleted });
+    }) or die;
+
+    MogileFS::register_worker_command('rename_if_no_refs', sub {
+        my ($query, $dmid, $dkey, $new_dkey) = @_;
+        my $dbh = Mgd::get_dbh();
+
+        local $@;
+        eval { $dbh->do("LOCK TABLES file_ref WRITE"); };
+        if ($@ || $dbh->err) {
+            eval { $dbh->do("UNLOCK TABLES"); };
+            return $query->err_line("rename_if_no_refs_failed");
+        }
+
+        my ($count) = eval { $dbh->selectrow_array("SELECT COUNT(*) FROM file_ref WHERE dmid = ? AND dkey = ?", {}, $dmid, $dkey) };
+        if ($@ || $dbh->err) {
+            eval { $dbh->do("UNLOCK TABLES"); };
+            return $query->err_line("rename_if_no_refs_failed");
+        }
+
+        if ($count != 0) {
+            eval { $dbh->do("UNLOCK TABLES"); };
+            return $query->ok_line({files_outstanding => $count});
+        }
+
+        my $updated = eval { $dbh->do("UPDATE file SET dkey = ? WHERE dmid = ? AND dkey = ?", {}, $new_dkey, $dmid, $dkey); };
+        if ($@ || $dbh->err) {
+            eval { $dbh->do("UNLOCK TABLES"); };
+            return $query->err_line("rename_if_no_refs_failed");
+        }
+        eval { $dbh->do("UNLOCK TABLES"); };
+
+        $query->ok_line({files_outstanding => 0, updated => $updated});
+    }) or die;
+}
 
 1;
 __END__
@@ -73,11 +102,11 @@ If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-Dave Lambley, E<lt>davel@E<gt>
+Dave Lambley, E<lt>davel@state51.co.ukE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2012 by Dave Lambley
+Copyright (C) 2012 Oscar Music and Media
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.10.1 or,
